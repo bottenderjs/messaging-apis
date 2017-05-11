feab4d756bcbd573d974d569b53bf979d4344de4
C.T. Lin <chentsulin@gmail.com>
2017-05-11 13:40:29 +0800

    build scripts

---
+++ scripts/_getPackages.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const PACKAGES_DIR = path.resolve(__dirname, '../packages');
+
+// Get absolute paths of all directories under packages/*
+module.exports = function getPackages() {
+  return fs
+    .readdirSync(PACKAGES_DIR)
+    .map(file => path.resolve(PACKAGES_DIR, file))
+    .filter(f => fs.lstatSync(path.resolve(f)).isDirectory());
+};
