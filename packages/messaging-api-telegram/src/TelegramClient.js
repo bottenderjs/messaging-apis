0198885328752ba2e0efc112a4e4e2bcf62c5100
xxhomey19 <xxhomey19@gmail.com>
2017-07-11 11:27:29 +0800

    add optional parameters in telegram api

--- packages/messaging-api-telegram/src/TelegramClient.js
+++ packages/messaging-api-telegram/src/TelegramClient.js
@@ -58,64 +58,71 @@ export default class TelegramClient {
   /**
    * https://core.telegram.org/bots/api#sendmessage
    */
-  sendMessage = (chatId: string, text: string) =>
+  sendMessage = (chatId: string, text: string, options?: Object) =>
     this._request('/sendMessage', {
       chat_id: chatId,
       text,
+      ...options,
     });
 
   /**
    * https://core.telegram.org/bots/api#sendphoto
    */
-  sendPhoto = (chatId: string, photo: string) =>
+  sendPhoto = (chatId: string, photo: string, options?: Object) =>
     this._request('/sendPhoto', {
       chat_id: chatId,
       photo,
+      ...options,
     });
 
   /**
    * https://core.telegram.org/bots/api#sendaudio
    */
-  sendAudio = (chatId: string, audio: string) =>
+  sendAudio = (chatId: string, audio: string, options?: Object) =>
     this._request('/sendAudio', {
       chat_id: chatId,
       audio,
+      ...options,
     });
 
   /**
    * https://core.telegram.org/bots/api#senddocument
    */
-  sendDocument = (chatId: string, document: string) =>
+  sendDocument = (chatId: string, document: string, options?: Object) =>
     this._request('/sendDocument', {
       chat_id: chatId,
       document,
+      ...options,
     });
 
   /**
    * https://core.telegram.org/bots/api#sendsticker
    */
-  sendSticker = (chatId: string, sticker: string) =>
+  sendSticker = (chatId: string, sticker: string, options?: Object) =>
     this._request('/sendSticker', {
       chat_id: chatId,
       sticker,
+      ...options,
     });
 
   /**
    * https://core.telegram.org/bots/api#sendvideo
    */
-  sendVideo = (chatId: string, video: string) =>
+  sendVideo = (chatId: string, video: string, options?: Object) =>
     this._request('/sendVideo', {
       chat_id: chatId,
       video,
+      ...options,
     });
 
   /**
    * https://core.telegram.org/bots/api#sendvoice
    */
-  sendVoice = (chatId: string, voice: string) =>
+  sendVoice = (chatId: string, voice: string, options?: Object) =>
     this._request('/sendVoice', {
       chat_id: chatId,
       voice,
+      ...options,
     });
 
   /**
@@ -132,12 +139,14 @@ export default class TelegramClient {
    */
   sendLocation = (
     chatId: string,
-    { latitude, longitude }: { latitude: number, longitude: number }
+    { latitude, longitude }: { latitude: number, longitude: number },
+    options?: Object
   ) =>
     this._request('/sendLocation', {
       chat_id: chatId,
       latitude,
       longitude,
+      ...options,
     });
 
   /**
@@ -150,7 +159,8 @@ export default class TelegramClient {
       longitude,
       title,
       address,
-    }: { latitude: number, longitude: number, title: string, address: string }
+    }: { latitude: number, longitude: number, title: string, address: string },
+    options?: Object
   ) =>
     this._request('/sendVenue', {
       chat_id: chatId,
@@ -158,6 +168,7 @@ export default class TelegramClient {
       longitude,
       title,
       address,
+      ...options,
     });
 
   /**
@@ -165,12 +176,14 @@ export default class TelegramClient {
    */
   sendContact = (
     chatId: string,
-    { phoneNumber, firstName }: { phoneNumber: string, firstName: string }
+    { phoneNumber, firstName }: { phoneNumber: string, firstName: string },
+    options?: Object
   ) =>
     this._request('/sendContact', {
       chat_id: chatId,
       phone_number: phoneNumber,
       first_name: firstName,
+      ...options,
     });
 
   /**
