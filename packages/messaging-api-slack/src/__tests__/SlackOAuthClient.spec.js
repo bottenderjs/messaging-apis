9800057e53d884e27999679234156c8272e566b4
tw0517tw <tw0517tw@gmail.com>
2017-08-03 16:09:01 +0800

    add slack oauth api test

--- packages/messaging-api-slack/src/__tests__/SlackOAuthClient.spec.js
+++ packages/messaging-api-slack/src/__tests__/SlackOAuthClient.spec.js
@@ -70,6 +70,74 @@ describe('#getHTTPClient', () => {
   });
 });
 
+describe('#callMethod', () => {
+  it('should call slack api', async () => {
+    const { client, mock } = createMock();
+
+    const reply = {
+      ok: true,
+      ts: '1405895017.000506',
+      channel: 'C024BE91L',
+      message: {},
+    };
+
+    mock
+      .onPost(
+        '/chat.postMessage',
+        querystring.stringify({
+          channel: CHANNEL,
+          text: 'hello',
+          token: TOKEN,
+        }),
+        {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      )
+      .reply(200, reply);
+
+    const res = await client.callMethod('chat.postMessage', {
+      channel: CHANNEL,
+      text: 'hello',
+    });
+
+    expect(res).toEqual(reply);
+  });
+
+  it('should throw if slack api return not ok', async () => {
+    expect.assertions(1);
+    const { client, mock } = createMock();
+
+    const reply = {
+      ok: false,
+      error: 'something wrong',
+      ts: '1405895017.000506',
+    };
+
+    mock
+      .onPost(
+        '/chat.postMessage',
+        querystring.stringify({
+          channel: CHANNEL,
+          text: 'hello',
+          token: TOKEN,
+        }),
+        {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      )
+      .reply(200, reply);
+
+    try {
+      await client.callMethod('chat.postMessage', {
+        channel: CHANNEL,
+        text: 'hello',
+      });
+    } catch (e) {
+      expect(e).toEqual(new Error('something wrong'));
+    }
+  });
+});
+
 describe('#postMessage', () => {
   it('should call chat.postMessage with channel and text', async () => {
     const { client, mock } = createMock();
@@ -199,6 +267,126 @@ describe('#getUserList', () => {
       .reply(200, reply);
 
     const res = await client.getUserList();
+
+    expect(res).toEqual(members);
+  });
+});
+
+describe('#getAllUserList', () => {
+  it('should call users.list api', async () => {
+    const { client, mock } = createMock();
+
+    const members = [
+      {
+        id: 'U023BECGF',
+        team_id: 'T021F9ZE2',
+        name: 'bobby',
+        deleted: false,
+        color: '9f69e7',
+        real_name: 'Bobby Tables',
+        tz: 'America/Los_Angeles',
+        tz_label: 'Pacific Daylight Time',
+        tz_offset: -25200,
+        profile: {
+          avatar_hash: 'ge3b51ca72de',
+          current_status: ':mountain_railway: riding a train',
+          first_name: 'Bobby',
+          last_name: 'Tables',
+          real_name: 'Bobby Tables',
+          email: 'bobby@slack.com',
+          skype: 'my-skype-name',
+          phone: '+1 (123) 456 7890',
+          image_24: 'https://...',
+          image_32: 'https://...',
+          image_48: 'https://...',
+          image_72: 'https://...',
+          image_192: 'https://...',
+        },
+        is_admin: true,
+        is_owner: true,
+        updated: 1490054400,
+        has_2fa: false,
+      },
+      {
+        id: 'W07QCRPA4',
+        team_id: 'T0G9PQBBK',
+        name: 'glinda',
+        deleted: false,
+        color: '9f69e7',
+        real_name: 'Glinda Southgood',
+        tz: 'America/Los_Angeles',
+        tz_label: 'Pacific Daylight Time',
+        tz_offset: -25200,
+        profile: {
+          avatar_hash: '8fbdd10b41c6',
+          image_24: 'https://a.slack-edge.com...png',
+          image_32: 'https://a.slack-edge.com...png',
+          image_48: 'https://a.slack-edge.com...png',
+          image_72: 'https://a.slack-edge.com...png',
+          image_192: 'https://a.slack-edge.com...png',
+          image_512: 'https://a.slack-edge.com...png',
+          image_1024: 'https://a.slack-edge.com...png',
+          image_original: 'https://a.slack-edge.com...png',
+          first_name: 'Glinda',
+          last_name: 'Southgood',
+          title: 'Glinda the Good',
+          phone: '',
+          skype: '',
+          real_name: 'Glinda Southgood',
+          real_name_normalized: 'Glinda Southgood',
+          email: 'glenda@south.oz.coven',
+        },
+        is_admin: true,
+        is_owner: false,
+        is_primary_owner: false,
+        is_restricted: false,
+        is_ultra_restricted: false,
+        is_bot: false,
+        updated: 1480527098,
+        has_2fa: false,
+      },
+    ];
+
+    const reply1 = {
+      ok: true,
+      members: [members[0]],
+      cache_ts: 1498777272,
+      response_metadata: {
+        next_cursor: 'cursor1',
+      },
+    };
+
+    const reply2 = {
+      ok: true,
+      members: [members[1]],
+      cache_ts: 1498777272,
+    };
+
+    mock
+      .onPost(
+        '/users.list',
+        querystring.stringify({
+          cursor: undefined,
+          token: TOKEN,
+        }),
+        {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      )
+      .replyOnce(200, reply1)
+      .onPost(
+        '/users.list',
+        querystring.stringify({
+          cursor: 'cursor1',
+          token: TOKEN,
+        }),
+        {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      )
+      .replyOnce(200, reply2);
+
+    const res = await client.getAllUserList();
 
     expect(res).toEqual(members);
   });
