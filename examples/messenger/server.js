61e75de9b5623832017e31d7b6a3ce09a6d86d85
C.T. Lin <chentsulin@gmail.com>
2017-09-18 16:28:43 +0800

    Added a messenger example

---
+++ examples/messenger/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MessengerClient } = require('messaging-api-messenger');
+
+const server = express();
+const client = MessengerClient.connect(process.env.ACCESS_TOKEN);
+
+server.use(bodyParser.json());
+
+server.get('/', (req, res) => {
+  if (
+    req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === process.env.VERIFY_TOKEN
+  ) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    console.error('Failed validation. Make sure the validation tokens match.');
+    res.sendStatus(403);
+  }
+});
+
+server.post('/', (req, res) => {
+  const event = req.body.entry[0].messaging[0];
+  const userId = event.sender.id;
+  const text = event.message.text;
+  client.sendText(userId, text);
+  res.sendStatus(200);
+});
+
+module.exports = server;
