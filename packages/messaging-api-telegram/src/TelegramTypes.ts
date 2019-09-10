485e74c5e613f27f5b831da1f713e7fa8060af33
chentsulin <chentsulin@gmail.com>
2019-09-10 22:44:36 +0800

    fix types in Telegram

--- packages/messaging-api-telegram/src/TelegramTypes.ts
+++ packages/messaging-api-telegram/src/TelegramTypes.ts
@@ -9,3 +9,66 @@ export type ChatAction =
   | 'find_location'
   | 'record_video_note'
   | 'upload_video_note';
+
+export type LoginUrl = {
+  url: string;
+  forward_text?: string;
+  bot_username?: string;
+  request_write_access?: boolean;
+};
+
+export type CallbackGame = any;
+
+export type InlineKeyboardButton = {
+  text: string;
+  url?: string;
+  login_url?: LoginUrl;
+  callback_data?: string;
+  switch_inline_query?: string;
+  switch_inline_query_current_chat?: string;
+  callback_game?: CallbackGame;
+  pay?: boolean;
+};
+
+export type InlineKeyboardMarkup = {
+  inline_keyboard: InlineKeyboardButton[][];
+};
+
+export type KeyboardButton = {
+  text: string;
+  request_contact?: boolean;
+  request_location?: boolean;
+};
+
+export type ReplyKeyboardMarkup = {
+  keyboard: KeyboardButton[][];
+  resize_keyboard?: boolean;
+  one_time_keyboard?: boolean;
+  selective?: boolean;
+};
+
+export type ReplyKeyboardRemove = {
+  remove_keyboard: boolean;
+  selective?: boolean;
+};
+
+export type ForceReply = {
+  force_reply: boolean;
+  selective?: boolean;
+};
+
+export type User = {
+  chat_id: number | string;
+  text: string;
+  parse_mode?: string;
+  disable_web_page_preview?: boolean;
+  disable_notification?: boolean;
+  reply_to_message_id?: number;
+  reply_markup?: InlineKeyboardMarkup | ReplyKeyboardMarkup | ReplyKeyboardRemove | ForceReply;
+};
+
+export type File = {
+  file_id: string;
+  file_size?: number;
+  file_path: string;
+};
