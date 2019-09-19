b2eedb5c96e0994aea7e8e5f699df5517c07bcfa
etrex kuo <et284vu065k3@gmail.com>
2019-09-19 15:37:24 +0800

    docs: add description on members of SetWebhookOption

--- packages/messaging-api-telegram/src/TelegramTypes.ts
+++ packages/messaging-api-telegram/src/TelegramTypes.ts
@@ -999,7 +999,20 @@ export type GameHighScore = {
 };
 
 export type SetWebhookOption = {
+  /**
+   * not supported yet.
+   */
   certificate?: string;
+
+  /**
+   * Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery, 1-100. Defaults to 40. Use lower values to limit the load on your bot‘s server, and higher values to increase your bot’s throughput.
+   */
   maxConnections?: number;
+
+  /**
+   * List the types of updates you want your bot to receive. For example, specify [“message”, “edited_channel_post”, “callback_query”] to only receive updates of these types. See Update for a complete list of available update types. Specify an empty list to receive all updates regardless of type (default). If not specified, the previous setting will be used.
+   *
+   * Please note that this parameter doesn't affect updates created before the call to the setWebhook, so unwanted updates may be received for a short period of time.
+   */
   allowedUpdates?: string[];
 };
