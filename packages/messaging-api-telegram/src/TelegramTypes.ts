540f5f7a0974709ad19ace3f0934987e24566b83
etrex kuo <et284vu065k3@gmail.com>
2019-09-19 15:32:28 +0800

    docs: add comments for vscode signature help

--- packages/messaging-api-telegram/src/TelegramTypes.ts
+++ packages/messaging-api-telegram/src/TelegramTypes.ts
@@ -12,13 +12,43 @@ export type Update = {
   poll?: Poll;
 };
 
+/**
+ * Contains information about the current status of a webhook.
+ */
 export type WebhookInfo = {
+  /**
+   * Webhook URL, may be empty if webhook is not set up
+   */
   url: string;
+
+  /**
+   * True, if a custom certificate was provided for webhook certificate checks
+   */
   has_custom_certificate: boolean;
+
+  /**
+   * Number of updates awaiting delivery
+   */
   pending_update_count: number;
+
+  /**
+   * Optional. Unix time for the most recent error that happened when trying to deliver an update via webhook
+   */
   last_error_date?: number;
+
+  /**
+   * Optional. Error message in human-readable format for the most recent error that happened when trying to deliver an update via webhook
+   */
   last_error_message?: string;
+
+  /**
+   * Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+   */
   max_connections?: number;
+
+  /**
+   * Optional. A list of update types the bot is subscribed to. Defaults to all update types
+   */
   allowed_updates?: string[];
 };
 
