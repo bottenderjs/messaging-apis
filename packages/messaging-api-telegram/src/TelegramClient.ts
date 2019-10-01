4783593d663a2234e32a14eeeb2f13b766bff4c5
etrex kuo <et284vu065k3@gmail.com>
2019-10-01 18:57:21 +0800

    chore: replace options type in TelegramClient#kickChatMember

--- packages/messaging-api-telegram/src/TelegramClient.ts
+++ packages/messaging-api-telegram/src/TelegramClient.ts
@@ -627,16 +627,24 @@ export default class TelegramClient {
   }
 
   /**
+   * Use this method to kick a user from a group, a supergroup or a channel. In the case of supergroups and channels, the user will not be able to return to the group on their own using invite links, etc., unless unbanned first. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Returns True on success.
+   *
+   * Note: In regular groups (non-supergroups), this method will only work if the ‘All Members Are Admins’ setting is off in the target group. Otherwise members may only be removed by the group's creator or by the member that added them.
+   *
+   * @param chatId Unique identifier for the target group or username of the target supergroup or channel (in the format @channelusername)
+   * @param userId Unique identifier of the target user
+   * @param options.untilDate Date when the user will be unbanned, unix time. If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever
+   *
    * - https://core.telegram.org/bots/api#kickchatmember
    */
   kickChatMember(
-    chatId: string,
-    userId: string,
-    options?: Record<string, any>
+    chatId: string | number,
+    userId: number,
+    options?: Type.KickChatMemberOption
   ): Promise<boolean> {
     return this._request('/kickChatMember', {
-      chat_id: chatId,
-      user_id: userId,
+      chatId,
+      userId,
       ...options,
     });
   }
