cb8d0398a07c1abef9e18856187316b1e9981b3d
etrex kuo <et284vu065k3@gmail.com>
2019-10-01 18:44:52 +0800

    docs: add docs for TelegramClient#getFile

--- packages/messaging-api-telegram/src/TelegramClient.ts
+++ packages/messaging-api-telegram/src/TelegramClient.ts
@@ -604,11 +604,15 @@ export default class TelegramClient {
   }
 
   /**
+   * Use this method to get basic info about a file and prepare it for downloading. For the moment, bots can download files of up to 20MB in size. On success, a File object is returned. The file can then be downloaded via the link https://api.telegram.org/file/bot<token>/<file_path>, where <file_path> is taken from the response. It is guaranteed that the link will be valid for at least 1 hour. When the link expires, a new one can be requested by calling getFile again.
+   *
+   * @param fileId File identifier to get info about
+   *
    * - https://core.telegram.org/bots/api#getfile
    */
   getFile(fileId: string): Promise<Type.File> {
     return this._request('/getFile', {
-      file_id: fileId,
+      fileId,
     });
   }
 
