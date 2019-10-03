4e72281106dc61cccacc7c271f31dc35f2b1d037
etrex kuo <et284vu065k3@gmail.com>
2019-10-03 15:48:06 +0800

    docs: add docs for TelegramClient#deleteChatPhoto

--- packages/messaging-api-telegram/src/TelegramClient.ts
+++ packages/messaging-api-telegram/src/TelegramClient.ts
@@ -760,11 +760,17 @@ export default class TelegramClient {
   // TODO: implement setChatPhoto
 
   /**
-   * - https://core.telegram.org/bots/api#deleteChatPhoto
+   * Use this method to delete a chat photo. Photos can't be changed for private chats. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Returns True on success.
+   *
+   * Note: In regular groups (non-supergroups), this method will only work if the ‘All Members Are Admins’ setting is off in the target group.
+   *
+   * @param chatId Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+   *
+   * - https://core.telegram.org/bots/api#deletechatphoto
    */
-  deleteChatPhoto(chatId: string): Promise<boolean> {
+  deleteChatPhoto(chatId: string | number): Promise<boolean> {
     return this._request('/deleteChatPhoto', {
-      chat_id: chatId,
+      chatId,
     });
   }
 
