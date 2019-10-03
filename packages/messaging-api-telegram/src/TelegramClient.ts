3487cc698827cca2a1653f4c3c7851dae4e9d7e1
etrex kuo <et284vu065k3@gmail.com>
2019-10-03 18:04:57 +0800

    docs: add docs for TelegramClient#unpinChatMessage

--- packages/messaging-api-telegram/src/TelegramClient.ts
+++ packages/messaging-api-telegram/src/TelegramClient.ts
@@ -831,11 +831,15 @@ export default class TelegramClient {
   }
 
   /**
-   * - https://core.telegram.org/bots/api#unpinChatMessage
+   * Use this method to unpin a message in a group, a supergroup, or a channel. The bot must be an administrator in the chat for this to work and must have the ‘can_pin_messages’ admin right in the supergroup or ‘can_edit_messages’ admin right in the channel. Returns True on success.
+   *
+   * @param chatId Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+   *
+   * - https://core.telegram.org/bots/api#unpinchatmessage
    */
-  unpinChatMessage(chatId: string): Promise<boolean> {
+  unpinChatMessage(chatId: string | number): Promise<boolean> {
     return this._request('/unpinChatMessage', {
-      chat_id: chatId,
+      chatId,
     });
   }
 
