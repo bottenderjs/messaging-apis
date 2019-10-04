56bead59efe2030f5f5d2f757d5f6ed11dc7026e
etrex kuo <et284vu065k3@gmail.com>
2019-10-04 14:55:21 +0800

    docs: add docs for TelegramClient#deleteMessage

--- packages/messaging-api-telegram/src/TelegramClient.ts
+++ packages/messaging-api-telegram/src/TelegramClient.ts
@@ -1014,12 +1014,21 @@ export default class TelegramClient {
   // TODO: implement stopPoll
 
   /**
+   * Use this method to delete a message, including service messages, with the following limitations:
+   * - A message can only be deleted if it was sent less than 48 hours ago.
+   * - Bots can delete outgoing messages in private chats, groups, and supergroups.
+   * - Bots can delete incoming messages in private chats.
+   * - Bots granted can_post_messages permissions can delete outgoing messages in channels.
+   * - If the bot is an administrator of a group, it can delete any message there.
+   * - If the bot has can_delete_messages permission in a supergroup or a channel, it can delete any message there.
+   * Returns True on success.
+   *
    * - https://core.telegram.org/bots/api#deletemessage
    */
-  deleteMessage(chatId: string, messageId: number): Promise<boolean> {
+  deleteMessage(chatId: string | number, messageId: number): Promise<boolean> {
     return this._request('/deleteMessage', {
-      chat_id: chatId,
-      message_id: messageId,
+      chatId,
+      messageId,
     });
   }
 
