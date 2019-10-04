c2e29cf1e16a3d99b247457ba5167f2d9124dabf
etrex kuo <et284vu065k3@gmail.com>
2019-10-04 19:53:08 +0800

    feat: implement TelegramClient#answerCallbackQuery

--- packages/messaging-api-telegram/src/TelegramTypes.ts
+++ packages/messaging-api-telegram/src/TelegramTypes.ts
@@ -2292,6 +2292,30 @@ export type AnswerPreCheckoutQueryOption = {
   errorMessage?: string;
 };
 
+export type AnswerCallbackQueryOption = {
+  /**
+   * Text of the notification. If not specified, nothing will be shown to the user, 0-200 characters
+   */
+  text?: string;
+
+  /**
+   * If true, an alert will be shown by the client instead of a notification at the top of the chat screen. Defaults to false.
+   */
+  showAlert?: boolean;
+
+  /**
+   * URL that will be opened by the user's client. If you have created a Game and accepted the conditions via @Botfather, specify the URL that opens your game – note that this will only work if the query comes from a callback_game button.
+   *
+   * Otherwise, you may use links like t.me/your_bot?start=XXXX that open your bot with a parameter.
+   */
+  url?: string;
+
+  /**
+   * The maximum amount of time in seconds that the result of the callback query may be cached client-side. Telegram apps will support caching starting in version 3.14. Defaults to 0.
+   */
+  cacheTime?: number;
+};
+
 export type SendGameOption = {
   /**
    * Sends the message silently. Users will receive a notification with no sound.
