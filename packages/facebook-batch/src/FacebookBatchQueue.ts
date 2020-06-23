12f02722a0542880c95e72d55c322c5d3c23a115
chentsulin <chentsulin@gmail.com>
2020-06-23 22:06:33 +0800

    chore(facebook-batch): refactor and improve types, comments

--- packages/facebook-batch/src/FacebookBatchQueue.ts
+++ packages/facebook-batch/src/FacebookBatchQueue.ts
@@ -16,59 +16,111 @@ const MAX_BATCH_SIZE = 50;
 const alwaysTrue = (): true => true;
 
 export default class FacebookBatchQueue {
-  _client: MessengerClient;
+  /**
+   * The queue to store facebook requests.
+   */
+  readonly queue: QueueItem[];
 
-  _queue: QueueItem[];
+  private client: MessengerClient;
 
-  _delay: number;
+  private delay: number;
 
-  _shouldRetry: (err: BatchRequestErrorInfo) => boolean;
+  private shouldRetry: (err: BatchRequestErrorInfo) => boolean;
 
-  _retryTimes: number;
+  private retryTimes: number;
 
-  _timeout: NodeJS.Timeout;
+  private timeout: NodeJS.Timeout;
 
+  /**
+   *
+   * @param client - A MessengerClient or FacebookClient instance.
+   * @param options - Optional batch config.
+   *
+   * @example
+   * ```js
+   * const client = new MessengerClient();
+   * new FacebookBatchQueue(client);
+   *
+   * new FacebookBatchQueue(client, {
+   *   delay: 3000,
+   *   shouldRetry: () => true,
+   *   retryTimes: 3,
+   * });
+   * ```
+   */
   constructor(client: MessengerClient, options: BatchConfig = {}) {
     invariant(
       client,
       'Must provide a MessengerClient or FacebookClient instance'
     );
 
-    this._client = client;
-    this._queue = [];
-    this._delay = options.delay ?? 1000;
-    this._shouldRetry = options.shouldRetry ?? alwaysTrue;
-    this._retryTimes = options.retryTimes ?? 0;
+    this.queue = [];
 
-    this._timeout = setTimeout(() => this.flush(), this._delay);
+    this.client = client;
+    this.delay = options.delay ?? 1000;
+    this.shouldRetry = options.shouldRetry ?? alwaysTrue;
+    this.retryTimes = options.retryTimes ?? 0;
+
+    this.timeout = setTimeout(() => this.flush(), this.delay);
   }
 
-  get queue(): QueueItem[] {
-    return this._queue;
-  }
-
-  push<T extends JsonObject = JsonObject>(request: BatchRequest): Promise<T> {
+  /**
+   * Pushes a facebook request into the queue.
+   *
+   * @param request - The request to be queued.
+   * @returns A promise resolves the response of the request.
+   *
+   * @example
+   * ```js
+   * await bq.push({
+   *   method: 'POST',
+   *   relativeUrl: 'me/messages',
+   *   body: {
+   *     messagingType: 'UPDATE',
+   *     recipient: 'PSID',
+   *     message: { text: 'Hello World' },
+   *   },
+   * });
+   * //=> {
+   *   recipientId: '...',
+   *   messageId: '...',
+   * }
+   * ```
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  push<T extends JsonObject = any>(request: BatchRequest): Promise<T> {
     const promise = new Promise((resolve, reject) => {
-      this._queue.push({ request, resolve, reject });
+      this.queue.push({ request, resolve, reject });
     });
 
-    if (this._queue.length >= MAX_BATCH_SIZE) {
+    if (this.queue.length >= MAX_BATCH_SIZE) {
       this.flush();
     }
 
     return promise as Promise<T>;
   }
 
+  /**
+   * Flushes the queue proactively.
+   *
+   * @remarks
+   * This queue has a timer to flush items at a time interval, so normally you don't need to call this method.
+   *
+   * @example
+   * ```js
+   * await bq.flush();
+   * ```
+   */
   async flush(): Promise<void> {
-    const items = this._queue.splice(0, MAX_BATCH_SIZE);
+    const items = this.queue.splice(0, MAX_BATCH_SIZE);
 
-    clearTimeout(this._timeout);
-    this._timeout = setTimeout(() => this.flush(), this._delay);
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => this.flush(), this.delay);
 
     if (items.length < 1) return;
 
     try {
-      const responses = await this._client.sendBatch(
+      const responses = await this.client.sendBatch(
         items.map((item) => item.request)
       );
 
@@ -84,16 +136,16 @@ export default class FacebookBatchQueue {
           request,
         };
 
-        if (retry < this._retryTimes && this._shouldRetry(err)) {
-          this._queue.push({ request, resolve, reject, retry: retry + 1 });
+        if (retry < this.retryTimes && this.shouldRetry(err)) {
+          this.queue.push({ request, resolve, reject, retry: retry + 1 });
         } else {
           reject(new BatchRequestError(err));
         }
       });
     } catch (err) {
       items.forEach(({ request, resolve, reject, retry = 0 }) => {
-        if (retry < this._retryTimes && this._shouldRetry(err)) {
-          this._queue.push({ request, resolve, reject, retry: retry + 1 });
+        if (retry < this.retryTimes && this.shouldRetry(err)) {
+          this.queue.push({ request, resolve, reject, retry: retry + 1 });
         } else {
           reject(err);
         }
@@ -101,7 +153,15 @@ export default class FacebookBatchQueue {
     }
   }
 
+  /**
+   * Stops the internal timer.
+   *
+   * @example
+   * ```js
+   * bq.stop();
+   * ```
+   */
   stop(): void {
-    clearTimeout(this._timeout);
+    clearTimeout(this.timeout);
   }
 }
