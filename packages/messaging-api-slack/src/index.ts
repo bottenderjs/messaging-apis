73cf46eb5cee4e802c2674b4849a7abdc81da9c5
chentsulin <chentsulin@gmail.com>
2021-10-20 15:33:43 +0800

    removed(slack): SlackWebhookClient

--- packages/messaging-api-slack/src/index.ts
+++ packages/messaging-api-slack/src/index.ts
@@ -1,4 +1,3 @@
 export { default as SlackOAuthClient } from './SlackOAuthClient';
-export { default as SlackWebhookClient } from './SlackWebhookClient';
 
 export * as SlackTypes from './SlackTypes';
